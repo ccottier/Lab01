c2933781884041fca457ed50f34447ad7f5ee23a
ccottier <chrystiecottier@gmail.com>
2017-01-26 20:36:04 -0800

    Update of Lab02

---
+++ app.js
@@ -0,0 +1,28 @@
+// function myFunction()
+// var userName = prompt('What\'s Your Name?');
+// alert('Hello' + userName + 'Thanks for coming to my site!');
+//
+//
+// var answerOne = prompt('In what state was I born?');
+// var myAnswerOne = ('Pennsylvania');
+// if (answerOne === myAnswerOne) {
+//   alert('Correct!  I was born in Philadelphia at the oldest hospital in the US which was founded by Ben Franklin');
+// } else {
+//   alert('Sorry, you are incorrect!');
+// }
+//
+//
+// var answerTwo = prompt('I was born on a holiday. Can you guess which one?');
+// var myAnswerTwo = ('New Year\'s Eve');
+// if var (answerTwo === myAnswerTwo) {
+//   alert('Correct! I was a last mintue tax deduction!');
+// }
+// else {
+//   alert('Wrong holiday!');
+// }
+
+function myFunction() {
+  var userName = prompt('What\'s Your Name?');
+  alert('Hello' + userName + 'Thanks for coming to my site.');
+  console.log('dave');
+}
