eac9f309efe10d7fef48bf76f254346cbc13d621
ccottier <chrystiecottier@gmail.com>
2017-01-27 17:23:56 -0800

    Added Questions

--- app.js
+++ app.js
@@ -1,51 +1,37 @@
-// function myFunction()
-// var userName = prompt('What\'s Your Name?');
-// alert('Hello' + userName + 'Thanks for coming to my site!');
-//
-//
-// var answerOne = prompt('In what state was I born?');
-// var myAnswerOne = ('Pennsylvania');
-// if (answerOne === myAnswerOne) {
-//   alert('Correct!  I was born in Philadelphia at the oldest hospital in the US which was founded by Ben Franklin');
-// } else {
-//   alert('Sorry, you are incorrect!');
-// }
-//
-//
-// var answerTwo = prompt('I was born on a holiday. Can you guess which one?');
-// var myAnswerTwo = ('New Year\'s Eve');
-// if var (answerTwo === myAnswerTwo) {
-//   alert('Correct! I was a last mintue tax deduction!');
-// }
-// else {
-//   alert('Wrong holiday!');
-// }
-
 function myFunction() {
   var userName = prompt('What\'s Your Name?');
-  alert('Hello '+ userName +'\! Thanks for coming to my first website.');
+  alert('Hello '+ userName +'\! Thanks for coming to my first website. Let\'s play a game so that you can get to know me a better.');
 }
 myFunction()
-
-// function guessingGame (){
-//   var userAnswer = prompt('Would you like to play a game to get to know me a little better?');
-// }
-//
-//   if (userAnswer === 'Yes' || 'Y'); {
-//     alert('Great! Let\'s begin.');
-// } else {
-//     alert('No worries!  Maybe next time?');
-//   }
-//
-// guessingGame()
 
 var answerOne = prompt('In which state was I born?');
 var myAnswerOne = 'Pennsylvania';
 
 if (answerOne === myAnswerOne) {
-  alert('You are correct! I was born in the oldest hospital in the United States which was founded by Ben Franklin.');
+  alert('You\'re correct! I was born in the oldest hospital in the United States which was founded by Ben Franklin.');
 } else {
   alert('Sorry, that was incorrect.  I was born in Philadelphia, PA.');
 }
-
 myAnswerOne()
+
+var answerTwo = prompt('Do I prefer dogs over cats?');
+var myAnswerTwo = 'No';
+
+if (answerTwo === myAnswerTwo) {
+  alert('Correct. I love cats.');
+} else {
+  alert('Nope. I much prefer cats over dogs.');
+}
+myAnswerTwo()
+
+
+var answerThree = prompt('Am I a morning person?');
+var myAnswerThree = 'No';
+
+if (answerThree === myAnswerThree) {
+  alert('Correct! I\'m more of a night owl.');
+} else {
+  alert('Incorrect. I\'m not an early bird.');
+}
+
+myAnswerThree()
